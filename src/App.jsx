151d7c6b55baa54cf9d7e0ba480916771e288f3d
Remy <shingiroremy303@gmail.com>
2026-03-25 18:14:38 +0200

    feat: ui buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-zinc-900">
-      <h1 className="text-4xl font-bold text-orange-500 underline">
-        Tailwind is Working!
-      </h1>
-    </div>
-  )
-}
+import React, {useState} from "react";
 
-export default App
+function App() {
+  const [display, setDisplay] = useState('0');
+
+  const getBtnStyle = (btn) => {
+    const base = 'h-20 text-2xl font-medium transition-all active:brightness-125';
+    if(['+', '-', 'x', '➗', '='].includes(btn)) return base + 'bg-orange-500 text-white';
+    if(['AC', '+/-', '%'].includes(btn)) return base + 'bg-zinc-400 text-black';
+
+    return base + 'bg-zinc-300 text-black';
+  };
+
+  const buttons = [
+    'AC', '+/-', '%', '÷',
+     '7', '8', '9', 'x',
+     '4', '5', '6', '-',
+     '0', '.', '='
+  ];
+
+}
