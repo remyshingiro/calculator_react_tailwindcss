c4c9a1df814cb40ed84e6715bb6dc91eff1e1e94
Remy <shingiroremy303@gmail.com>
2026-03-25 17:59:52 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-900">
+      <h1 className="text-4xl font-bold text-orange-500 underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
